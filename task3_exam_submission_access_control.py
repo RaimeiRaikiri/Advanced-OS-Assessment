ebd4ce05d0ac2fcd21b96ac3ebdef1ad64c0d72d
Michael Scutts <michaelscutts10@kali.domain>
2026-02-26 01:56:14 +0000

    feat(menu): menu function that prints menu options

--- task3_exam_submission_access_control.py
+++ task3_exam_submission_access_control.py
@@ -1,2 +1,10 @@
+def menu():
+        print("1 Submit an assignment")
+        print("2 Check if a file has been submitted")
+        print("3 List all submitted assignments")
+        print("4 Simulate login attempt")
+        print("5 Exit system")
+        print()
+
 def main():
 	pass
