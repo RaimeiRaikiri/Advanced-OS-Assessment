1c5cc886334b78f80a539ba0961fffd8041eb4e6
Michael Scutts <michaelscutts10@kali.domain>
2026-02-26 02:47:11 +0000

    feat(exit): added exit function, with Y/N confirmation. And inserted in 5th option of the main loop

--- task3_exam_submission_access_control.py
+++ task3_exam_submission_access_control.py
@@ -6,6 +6,22 @@ def menu():
         print("5 Exit system")
         print()
 
+def exit_system():
+        while True:
+                confirm = input("Are you sure you want to exit? (Enter Y to confirm, N to deny): ")
+                if confirm.capitalize() == "Y":
+                        print("Exit confirmed")
+                        exit()
+                elif confirm.capitalize() == "N":
+                        print("You have cancelled system exit")
+                        print()
+                        break
+                else:
+                        print("Invalid input, try again")
+                        print()
+                        continue
+
+
 def main():
 	print("Welcome to the secure examination and access control system!")
 	print()
@@ -15,27 +31,26 @@ def main():
 	# Create the log file if it doesn't exist
 	with open("submission_log.txt", "a+") as submission_file:
 
-                submission_file.seek(0)
+		submission_file.seek(0)
 
-                # Check if there is a heading in the log file, if not add one
-                if not submission_file.readline():
-                        submission_file.write(heading)
+		# Check if there is a heading in the log file, if not add one
+		if not submission_file.readline():
+			submission_file.write(heading)
 
-                # Ensure the previous check does not misalign the pointer
+		# Ensure the previous check does not misalign the pointer
 		submission_file.seek(0)  # Pointer reset to file start
-                submission_file.readline() # pointer to 2nd line
+		submission_file.readline() # pointer to 2nd line
 
-
-                # Take filename from each submission, add to list in memory 
-                for line in submission_file.readlines():
-                        values = line.split()
-                        try:
-                                if values[3]:  # Always 4th item of values due to submission log structure
-                                        all_files.append(values[3])
-                        except:
+		# Take filename from each submission, add to list in memory 
+		for line in submission_file.readlines():
+			values = line.split()
+			try:
+				if values[3]:  # Always 4th item of values due to submission log structure
+					all_files.append(values[3])
+			except:
 				# Do nothing
 				# Exception should only occur when there is no filename, and therefore no need to add a file
-                                pass
+				pass
 
 	while True:
 		menu()
@@ -52,4 +67,4 @@ def main():
 			case 4:
 				pass
 			case 5:
-				pass
+				exit_system()
