564ad0639f1eb562b2774390c0f86d83f71408c5
Michael Scutts <michaelscutts10@kali.domain>
2026-02-27 11:22:18 +0000

    feat(exit): added exit function, with Y/N confirmation, for job scheduler

--- task2_job_scheduler.py
+++ task2_job_scheduler.py
@@ -1,4 +1,18 @@
-
+def exit_system():
+	while True:
+		confirm = input("Are you sure you want to exit? (Enter Y to continue, N to deny): ")
+		
+		if confirm.capitalize() == "Y":
+			print("Exit confirmed")
+			exit()
+		elif confirm.capitalize() == "N":
+			print("You have cancelled system exit")
+			print()
+			break
+		else:
+			print("Invalid input, try again")
+			print()
+			continue
 def main()
 	pass
 
