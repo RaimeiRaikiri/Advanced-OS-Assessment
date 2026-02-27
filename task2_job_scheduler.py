d4e54e1cd54210078ac5105d3252380c2ff66a84
Michael Scutts <michaelscutts10@kali.domain>
2026-02-27 11:25:09 +0000

    feat(menu): added menu function that prints the available options for the job scheduler

--- task2_job_scheduler.py
+++ task2_job_scheduler.py
@@ -1,3 +1,11 @@
+def menu():
+	print("1 Submit an assignment")
+	print("2 Check if a file has been submitted")
+	print("3 List all submitted assignments")
+	print("4 Simulate login attempt")
+	print("5 Exit system")
+	print()
+
 def exit_system():
 	while True:
 		confirm = input("Are you sure you want to exit? (Enter Y to continue, N to deny): ")
@@ -13,6 +21,5 @@ def exit_system():
 			print("Invalid input, try again")
 			print()
 			continue
-def main()
+def main():
 	pass
-
