722064f6ba87262290bf842e7391b0b32307d30d
Michael Scutts <michaelscutts10@kali.domain>
2026-03-12 12:36:11 +0000

    feat(exit): added exit function with Y/N confirmation, added to the loop and fixed menu mistake

--- task3_exam_submission_access_control.sh
+++ task3_exam_submission_access_control.sh
@@ -3,9 +3,23 @@ echo "1 Submit an assignment"
 echo "2 Check if a file has been submitted"
 echo "3 List all submitted assignments"
 echo "4 Simulate login attempt"
+echo "5 Exit system"
 echo
 }
 
+exit_system(){
+while true; do
+
+	read -r -p "Are you sure you want to exit (Y to confirm, N to deny): " confirm
+	if [[ "$confirm"  == "Y" ]] || [[ "$confirm" == "y" ]]; then
+		echo "Shutting down. See you next time!" && exit
+	elif [[ "$confirm" == "N" ]] || [[ "$confirm" == "n" ]]; then
+		echo "You've chosen to stay in the system."
+		break
+	else
+		echo "Invalid choice, try again!"
+		continue
+}
 main_loop(){
 echo "Welcome to the secure examination and access control system!"
 echo
@@ -21,7 +35,7 @@ case "$choice" in
 	2) ;;
 	3) ;;
 	4) ;;
-	5) ;;
+	5) exit_system ;;
 	*) echo "Invalid choice" ;;
 	esac
 	echo
