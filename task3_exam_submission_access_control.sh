7220a9eb7d1515820db23d0510b9b006a20b2b85
Michael Scutts <michaelscutts10@kali.domain>
2026-03-12 12:26:33 +0000

    fix(menu): fixed incorrect use of python syntax

--- task3_exam_submission_access_control.sh
+++ task3_exam_submission_access_control.sh
@@ -1,7 +1,8 @@
-def menu(){
+menu(){
 	echo "1 Submit an assignment"
 	echo "2 Check if a file has been submitted"
 	echo "3 List all submitted assignments"
 	echo "4 Simulate login attempt"
 	echo
 }
+
