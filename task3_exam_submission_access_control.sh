9112ceb33af7d1630b7778c7fa470a38510bfb2d
Michael Scutts <michaelscutts10@kali.domain>
2026-03-12 12:31:06 +0000

    feat(main-loop): added general outline of mainloop

--- task3_exam_submission_access_control.sh
+++ task3_exam_submission_access_control.sh
@@ -1,8 +1,29 @@
 menu(){
-	echo "1 Submit an assignment"
-	echo "2 Check if a file has been submitted"
-	echo "3 List all submitted assignments"
-	echo "4 Simulate login attempt"
-	echo
+echo "1 Submit an assignment"
+echo "2 Check if a file has been submitted"
+echo "3 List all submitted assignments"
+echo "4 Simulate login attempt"
+echo
 }
 
+main_loop(){
+echo "Welcome to the secure examination and access control system!"
+echo
+
+while true; do 
+menu
+
+read -r -p "Select choice: " choice
+echo
+case "$choice" in
+
+	1) ;;
+	2) ;;
+	3) ;;
+	4) ;;
+	5) ;;
+	*) echo "Invalid choice" ;;
+	esac
+	echo
+done
+}
